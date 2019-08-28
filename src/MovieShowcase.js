a0daaa3aa51d617272c4e82dab19452abb2aaf5a
github username <github email address>
2019-08-28 11:07:17 -0400

    done but tests not passing

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -4,14 +4,30 @@ import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
 
-  generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+  // generateMovieCards = () => {
+  //
+  //   // console.log("generated movie cards displayed");
+  //   // map over your movieData array and return the correct
+  //   const movieComponents = movieData.map(movie => {
+  //     return <MovieCard title={movie.title} IMDBRating={movie.IMDBRating} genres={movie.genres} poster={movie.poster} />
+  //   })
+  //   console.log(movieData);
+  //
+  //   return movieComponents
+  // }
 
   render() {
+    const movieCardComponents = movieData.map(movie => {
+      // console.log(movie);
+      // return <MovieCard movie={movie} />
+      // BUG: creates a default MovieCard object with default props, then adds a movie key with value of {movie}.
+      // NOT Same as returning the below
+      return <MovieCard title={movie.title} IMDBRating={movie.IMDBRating} genres={movie.genres} poster={movie.poster} />
+
+    })
     return (
       <div id="movie-showcase">
-        {this.generateMovieCards()}
+        {movieCardComponents}
       </div>
     )
   }
--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,16 +12,28 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
+    if (this.props.IMDBRating === null) {
+      return <h4>'No Rating Found'</h4>
+    } else {
+      return <img src={imgMapper[this.props.IMDBRating]} alt="" />
+    }
   }
 
   render() {
+    // console.log("CardBack logging ⬇️");
+    // console.log(this.props);
+    // console.log("What is this? ⬇️");
+    // console.log(this);
+
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */
+          this.generateRatingElement()
+        }
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">{this.props.genresArr.join(', ')}</h5>
       </div>
     )
   }
